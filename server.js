171f89f12e6918999e588742a0f3abfb89406621
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-12-05 05:02:51 +0200

    add route is added

--- server.js
+++ server.js
@@ -7,13 +7,14 @@
 //? npm install body-parser
 //? npm install -g nodemon
 //? npm install url
+//? npm install mysql
 //TODO: to run the server type: nodemon server.js
 
 
 require('dotenv').config(); // for .env file
 const express = require("express"); // express module for routes
+const mysql = require("mysql") // mysql database
 const bodyParser = require("body-parser"); // for requestes parsing
-
 
 const favicon = require('serve-favicon'); // for icon
 //const chalk = require("chalk") // for console colors
@@ -31,6 +32,31 @@ app.use(favicon(__dirname + '/public/images/favicon.png')); // favicon location
 
 
 //? ---------------------------------------------< Database section >--------------------------------------------------------
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user:"root",
+//     password: "",
+//     database: "car_rental"
+// });
+
+// db.connect((err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(">>> Database connected !")
+//     }
+// });
+
+// let sql = "SELECT * FROM admin";
+// db.query(sql,(err,result)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(result)
+//     }
+// })
+
+
 
 
 //? -------------------------------------------< End of Database Section >-------------------------------------------------------
@@ -56,17 +82,45 @@ app.route("/")
         }
     });
 //? ---------------------------------------------< End of root route section >---------------------------------------------------
-//? ---------------------------------------------< Sign up route section >-------------------------------------------------------
+
+//? ---------------------------------------------< Admin route section >-------------------------------------------------------
 app.route("/admin")
     .get(function (req, res) {
         res.render("control/dashboard", { admin: "Amr ABSO" })
 
     })
     .post(function (req, res) {
-
         let menu_btn = req.body.control_btn;
-        console.log(menu_btn)
         res.render("control/" + menu_btn, { admin: "Amr ABSO" })
+    });
+
+app.route("/add")
+    .post(function (req, res) {
+        console.log(req.body.control_btn)
+        if (req.body.control_btn === "add_car") {
+            let new_car = {
+                company: req.body.company,
+                color: req.body.color,
+                stat: req.body.stat,
+                office: req.body.office,
+                model: req.body.model,
+                year: req.body.year,
+                miles: req.body.miles,
+                price: req.body.price,
+                lic_no: req.body.lic_no,
+                image_path: req.body.image,
+            };
+            console.log(new_car)
+        } else if (req.body.control_btn === "add_customer") {
+            let new_car = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                address: req.body.address,
+                phone: req.body.phone,
+            };
+            console.log(new_car)
+        }
     });
 //? -------------------------------------------< End of sign up route section >-------------------------------------------------
 
