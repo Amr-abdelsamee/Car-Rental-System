f3b954d43bc59fe84129b2ab7873ef5c34dde0e5
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-12-14 00:31:43 +0200

    DB schema is updated

--- DDL.sql
+++ DDL.sql
@@ -3,41 +3,47 @@ USE car_rental;
 CREATE TABLE cars(
     car_id int auto_increment PRIMARY KEY,
     company varchar(127) not null,
-    color varchar(127) not null,
-    status varchar(127) not null,
-    office varchar(127) not null,
     model varchar(127) not null,
-    year int not null,
+    lic_no varchar(11) not null UNIQUE,
+    color varchar(127) not null,
+    `status` varchar(127) not null,
+    `year` int not null,
     miles float not null,
     price int not null,
-    lic_no varchar(11) not null UNIQUE,
+    office_id int not null,
     image_path MEDIUMBLOB
 );
 CREATE TABLE customers(
     customer_id int auto_increment PRIMARY KEY, 
-    name varchar(255) not null,
-    password varchar(255) not null,
+    fname varchar(255) not null,
+    lname varchar(255) not null,
     email varchar(255) not null,
-    address varchar(255) not null,
+    `password` varchar(255) not null,
+    `address` varchar(255) not null,
     phone int not null
 );
 CREATE TABLE reservations(
     reserve_no int auto_increment PRIMARY KEY, 
-    customer_id int,
-    car_id int,
-    startD DATE not NULL,
-    endD DATE not NULL
+    customer_id int not null,
+    car_id int not null,
+    startD DATE not null,
+    endD DATE not null
 );
 CREATE TABLE admins(
-    password varchar(255) not null,
-    email varchar(255) not null PRIMARY KEY
+    email varchar(255) not null PRIMARY KEY,
+    `password` varchar(255) not null
 );
+CREATE TABLE offices(
+    office_id int auto_increment not null PRIMARY KEY, 
+    `location` varchar(255) not null
+);
+
 ALTER TABLE reservations ADD FOREIGN KEY (car_id) 
 REFERENCES cars(car_id);
 
 ALTER TABLE reservations ADD FOREIGN KEY (customer_id) 
 REFERENCES customers(customer_id);
 
-INSERT INTO admins(email, password) 
-VALUES ('admin','admin');
+ALTER TABLE cars ADD FOREIGN KEY (office_id) 
+REFERENCES offices(office_id);
 
--- DML.sql
+++ DML.sql
@@ -1,11 +1,54 @@
-USE CAR_RENTAL;
-INSERT INTO CARS VALUES (NULL,"BMW",'BLACK','ACTIVE','ALEXANDRIA','320i',2009,50000,30000,'A3B3B6',NULL);
-INSERT INTO CARS VALUES (NULL,"OPEL",'WHITE','ACTIVE','ALEXANDRIA','INSIGNIA',2019,5000,50000,'A5656D',NULL);
-INSERT INTO CARS VALUES (NULL,"MERCEDES",'GREEN','RENTED','ALEXANDRIA','GLA',2022,1000,100000,'A8754S',NULL);
-INSERT INTO CARS VALUES (NULL,"DAEWOO",'SILVER','OUT OF SERVICE','QUESNA','NUBIRA',2004,500000,10000,'Q8G83H',NULL);
-INSERT INTO CARS VALUES (NULL,"FIAT",'BLUE','RENTED','CAIRO','TIPO',2020,30000,25000,'C3B9B9',NULL);
-INSERT INTO CUSTOMERS VALUES(NULL,'adel','adel','adel@gmail.com','moh bek',01014066663);
-INSERT INTO CUSTOMERS VALUES(NULL,'abso','money','abso@hotmail.com','abu qir',01014022344);
-INSERT INTO CUSTOMERS VALUES(NULL,'s3s3','cash','s3s3@alexu.edu.eg','antoniadis park',01015998533);
-INSERT INTO RESERVATIONS VALUES(NULL,1,3,'2022-10-20','2023-2-1');
-INSERT INTO RESERVATIONS VALUES(NULL,3,5,'2022-12-5','2022-12-10');
+USE car_rental;
+INSERT INTO offices VALUES(NULL,'Alexandria');
+INSERT INTO offices VALUES(NULL, 'Aswan');
+INSERT INTO offices VALUES(NULL, 'Asyut');
+INSERT INTO offices VALUES(NULL, 'Beheira');
+INSERT INTO offices VALUES(NULL, 'Beni Suef');
+INSERT INTO offices VALUES(NULL, 'Cairo');
+INSERT INTO offices VALUES(NULL, 'Dakahlia');
+INSERT INTO offices VALUES(NULL, 'Damietta');
+INSERT INTO offices VALUES(NULL, 'Faiyum');
+INSERT INTO offices VALUES(NULL, 'Gharbia');
+INSERT INTO offices VALUES(NULL, 'Giza');
+INSERT INTO offices VALUES(NULL, 'Ismailia');
+INSERT INTO offices VALUES(NULL, 'Kafr el-Sheikh');
+INSERT INTO offices VALUES(NULL, 'Matruh');
+INSERT INTO offices VALUES(NULL, 'Minya');
+INSERT INTO offices VALUES(NULL, 'Monufia');
+INSERT INTO offices VALUES(NULL, 'New Valley');
+INSERT INTO offices VALUES(NULL, 'North Sinai');
+INSERT INTO offices VALUES(NULL, 'Port Said');
+INSERT INTO offices VALUES(NULL, 'Qalyubia');
+INSERT INTO offices VALUES(NULL, 'Qena');
+INSERT INTO offices VALUES(NULL, 'Red Sea');
+INSERT INTO offices VALUES(NULL, 'Al Sharqia');
+INSERT INTO offices VALUES(NULL, 'Sohag');
+INSERT INTO offices VALUES(NULL, 'South Sinai');
+INSERT INTO offices VALUES(NULL, 'Suez');
+INSERT INTO offices VALUES(NULL, 'Luxor');
+
+
+INSERT INTO CARS(car_id, company, model, lic_no, color, `status`, `year`, miles, price, office_id, image_path)
+        VALUES (NULL,"BMW",'320i','A3B3B6','BLACK','Active',2009,50000,30000,1,NULL);
+INSERT INTO CARS(car_id, company, model, lic_no, color, `status`, `year`, miles, price, office_id, image_path) 
+        VALUES (NULL,"OPEL",'INSIGNIA','A5656D','WHITE','Active',2019,50000,50000,1,NULL);
+INSERT INTO CARS(car_id, company, model, lic_no, color, `status`, `year`, miles, price, office_id, image_path) 
+VALUES (NULL,"OPEL",'astra','Adel56D','WHITE','Active',2019,10000,250000,1,NULL);
+
+
+INSERT INTO CUSTOMERS (customer_id, fname, lname, email, `password`, `address`, phone) 
+    VALUES(NULL,'adel','yasser','adel@','123','moh bek',01014066663);
+INSERT INTO CUSTOMERS (customer_id, fname, lname, email, `password`, `address`, phone) 
+    VALUES(NULL,'amr','abdelsamee','abso@','456','abu qir',01014022344);
+INSERT INTO CUSTOMERS (customer_id, fname, lname, email, `password`, `address`, phone) 
+    VALUES(NULL,'mohamed','saeed','saeed@','789','antoniadis park',01015998533);
+
+INSERT INTO RESERVATIONS(reserve_no, customer_id, car_id, startD, endD) VALUES(NULL,1,1,'2022-10-20','2023-2-1');
+INSERT INTO RESERVATIONS(reserve_no, customer_id, car_id, startD, endD) VALUES(NULL,2,3,'2022-12-5','2022-12-10');
+
+INSERT INTO admins(email, `password`) VALUES ('admin','admin');
+INSERT INTO admins(email, `password`) VALUES ('amr','1');
+INSERT INTO admins(email, `password`) VALUES ('saeed','2');
+INSERT INTO admins(email, `password`) VALUES ('adel','3');
+INSERT INTO admins(email, `password`) VALUES ('kim','4');
+INSERT INTO admins(email, `password`) VALUES ('slwa','5');
