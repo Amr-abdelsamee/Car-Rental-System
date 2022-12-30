c355d29809142b28a4359ac6a684349b4b4845ca
Amr Abdelsamee <67904417+Amr-abdelsamee@users.noreply.github.com>
2022-12-30 22:05:32 +0200

    car service schedule added

--- DDL.sql
+++ DDL.sql
@@ -17,10 +17,11 @@ CREATE TABLE customers(
     customer_id int auto_increment PRIMARY KEY, 
     fname varchar(255) NOT NULL,
     lname varchar(255) NOT NULL,
+    national_id int(20) NOT NULL UNIQUE,
     email varchar(255) NOT NULL UNIQUE ,
     `password` varchar(255) NOT NULL,
     `address` varchar(255) NOT NULL,
-    phone int NOT NULL
+    phone int(12) NOT NULL
 );
 CREATE TABLE reservations(
     reserve_no int auto_increment PRIMARY KEY, 
@@ -32,6 +33,14 @@ CREATE TABLE reservations(
     cost INT(11) NOT NULL,
     `rented` ENUM('Yes','No') NOT NULL
 );
+CREATE TABLE service_sche(
+    serv_no int auto_increment PRIMARY KEY, 
+    car_id int NOT NULL,
+    startD datetime NOT NULL,
+    endD datetime NOT NULL,
+    serv_date datetime DEFAULT current_timestamp() NOT NULL,
+);
+
 CREATE TABLE admins(
     email varchar(255) NOT NULL PRIMARY KEY,
     `password` varchar(255) NOT NULL
@@ -49,4 +58,6 @@ REFERENCES customers(customer_id);
 
 ALTER TABLE cars ADD FOREIGN KEY (office_id) 
 REFERENCES offices(office_id);
+
+
 
